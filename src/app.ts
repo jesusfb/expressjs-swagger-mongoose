f61f60c9bf48dbfe9cb64a5faeea81fba300acd8
Sridhar R <sridharofficialmail@gmail.com>
2023-03-08 20:57:19 +0530

    swagger config

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+
+import express,{Express} from 'express';
+import * as swaggerUi from "swagger-ui-express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import db from "./common/manager.ts/config";
+
+const port: number = 8000;
+mongoose.connect(db);
+const database = mongoose.connection;
+database.on("error", (error) => console.error());
+database.once("connected", () => console.log("Database Connected"));
+const app: Express = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
+
+const swaggerDocument = require('./../swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
