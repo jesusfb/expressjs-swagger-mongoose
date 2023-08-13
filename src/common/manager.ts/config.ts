439536bd89983672c106f8bfc9bcbc60c8499e75
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-13 13:01:19 -0700

    Update config.ts

--- src/common/manager.ts/config.ts
+++ src/common/manager.ts/config.ts
@@ -1,5 +1,5 @@
 const mongoString =
   // "mongodb+srv://british-empire:sri@british-empire.wcsq6jq.mongodb.net/sample-Inventory-System-Management?retryWrites=true&w=majority";
-  "mongodb+srv://british-empire:sri@british-empire.wcsq6jq.mongodb.net/Socket?retryWrites=true&w=majority";
+  "mongodb+srv://jesusfb:Dove3229-@cluster0.yx9sjqo.mongodb.net/Socket?retryWrites=true&w=majority";
 
 export default  mongoString ;
